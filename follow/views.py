10f8082d7d2bbf61fabcf48490d84099374e895f
migdracios <migdracios@gmail.com>
2022-06-01 17:28:01 +0900

    Add - 전체 유저 프론트엔드에 보여주기

--- follow/views.py
+++ follow/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import UserModel, FollowModel
 
 # Create your views here.
 def view_test_follow(request):
-    return render(request, 'follow/test_follow.html')
+    if request.method == 'GET':
+        all_user = UserModel.objects.all()
+        print(f'all_user -> {all_user}')
+        return render(request, 'follow/test_follow.html', {'all_user': all_user})
