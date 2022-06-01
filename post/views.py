636574b567e5b7969457b4c024fa7a7bee2a4927
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-06-01 15:19:33 +0900

    깃 이그노어

--- post/views.py
+++ post/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import PostModel
 # from datetime import datetime, timedelta
-from .forms import UploadFileForm
+
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -13,8 +14,11 @@ def post(request):
         return render(request, 'home.html', {'post': all_post})
     elif request.method == 'POST':
         # author = request.user
-        text = request.POST.get('desc_give','')
-        file = request.data.get('file')
+        # text = request.POST.get('desc_give','')
+        text = request.form['desc_give']
+        print(text)
+        file = request.files('file_give')
 
         print(text)
+        return HttpResponse("업로드가 잘되었습니다")
 
