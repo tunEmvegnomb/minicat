2ec7d51f84d0d9ab5088b7446c9004632a976724
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-01 16:02:27 +0900

    Add - 회원가입,로그인 urls와 프로젝트 urls 연결, models-views.py 생성

--- minicat/user/models.py
+++ minicat/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+class UserModel(models.Model):
+    class Meta:
+        db_table = "user"
+        
+    username = models.CharField(max_length=20, null=False) 
+    password = models.CharField(max_length=256, null=False)
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)
