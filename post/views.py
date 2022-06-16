f4e91adb2de66d3ff6baa6b4c11c292151f8d637
migdracios <migdracios@gmail.com>
2022-06-16 19:37:56 +0900

    Add - 고양이팩트 open api 가져오기

--- post/views.py
+++ post/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import PostModel
+import requests
 # from rest_framwork.views import API
 # from datetime import datetime, timedelta
 
@@ -19,3 +20,8 @@ def post(request):
         file = request.data.get('file')
         return HttpResponse("업로드가 잘되었습니다")
 
+def get_catfact(request):
+    request_catfact = requests.get('https://meowfacts.herokuapp.com/')
+    content_catfact = request_catfact.text.strip('{"data":}[""]')
+    print(f'content->{content_catfact}')
+    return content_catfact
